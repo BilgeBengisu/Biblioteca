72b5dc3fbb93df5c86fbe9c0077e672d2ed2ff7b
BilgeBengisu <76906763+BilgeBengisu@users.noreply.github.com>
2025-08-28 23:35:47 -0400

    redux implementation started, bootstrap included

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,26 @@ import Activate from './containers/Activate';
 import ResetPassword from './containers/ResetPassword';
 import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Layout from './hocs/Layout';
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/signup' element={<Signup />} />
-        <Route exact path='/activate/:uid/:token' element={<Activate />} />
-        <Route exact path='/reset-password' element={<ResetPassword />} />
-        <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
-      </Routes>
-    </Layout>
-  </Router>
+  <Provider store={(store)}>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/activate/:uid/:token' element={<Activate />} />
+          <Route exact path='/reset-password' element={<ResetPassword />} />
+          <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
+        </Routes>
+      </Layout>
+    </Router>
+  </Provider>
 
 );
 
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Login = () => (
-    <div>
-        Login
-    </div>
-);
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
 
-export default Login;
+    const { email, password } = formData;
+    
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log('Success');
+
+        // login(email, password);
+    }
+
+    // is the user authenticated?
+    // redirect them to the home page
+
+    return (
+        <div className="container mt-5">
+            <h1>Bienvenido a Biblioteca</h1>
+            <p>Inicia sesión en tu cuenta</p>
+            <form onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input 
+                        className="form-control" 
+                        type="email" 
+                        placeholder="Correo electrónico" 
+                        name="email" 
+                        value={email} 
+                        onChange={e => onChange(e)} 
+                        required 
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        className="form-control" 
+                        type="password" 
+                        placeholder="Contraseña" 
+                        name="password" 
+                        value={password} 
+                        onChange={e => onChange(e)} 
+                        minLength="6" 
+                        required 
+                    />
+                </div>
+                <button className="btn btn-primary" type="submit">Entrar</button>
+            </form>
+            <p className="mt-2">
+                No tenes una cuenta? <Link to="/signup">Registrese</Link>
+            </p>
+            <p className="mt-3">
+                Olvidaste tu contraseña? <Link to="/reset-password">Restablecer Contraseña</Link>
+            </p>
+        </div>
+    );
+};
+
+// const mapStateToProps = state => ({
+    //     isAuthenticated: state.auth.isAuthenticated
+// });
+
+export default connect(null, { })(Login);
+
